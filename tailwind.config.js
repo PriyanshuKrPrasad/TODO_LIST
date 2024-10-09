6ce10c31be5325ce41cbfd2d5f6001d44eb25098
Priyanshu Kumar Prasad <153725612+PriyanshuKrPrasad@users.noreply.github.com>
2024-10-10 01:31:07 +0530

    Readme

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// this project will start with 'npm run dev'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
